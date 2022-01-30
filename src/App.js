859673f9ae43afb6cd5a73db86e168cba868f625
jennibearduit <77873928+jennibearduit@users.noreply.github.com>
2022-01-30 15:09:07 +0800

    Add semester form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import Semester from './components/Semester'
-import { Button, Grid } from '@mui/material';
+import { Button, Card, Grid, Modal } from '@mui/material';
 import { useState } from 'react';
+import SemesterForm from './components/SemesterForm';
 
 const mockSemesters = [1, 2, 3, 4].map(i => `Semester ${i}`)
 const counter = 4
 
 const App = () => {
   const [semesters, setSemesters] = useState([...mockSemesters])
+  const [open, setOpen] = useState(false)
   console.log(semesters);
 
-  const handleClick = () => {
-    window.alert("Form not implemented yet! :)")
+  const handleOpen = () => {
+    setOpen(true);
   }
+
+  const handleClose = () => {
+    setOpen(false);
+  }
+
+  const handleSave = () => {
+    setOpen(false);
+  }
+
 
   return (
     <>
@@ -23,11 +34,27 @@ const App = () => {
           </Grid>
         ))}
         <Grid item xs={12} md={12}>
-          <Button onClick={handleClick} variant="contained">
+          <Button onClick={handleOpen} variant="contained">
             Add New Semester
           </Button>
         </Grid>
       </Grid>
+      <Modal open={open} onClose={handleClose}>
+        <Grid container alignItems="center" direction="row" justifyContent="center" spacing={2} padding={4}>
+          <Card>
+            <Grid container alignItems="center" direction="row" justifyContent="center" spacing={2} padding={4}>
+              <Grid item xs={12} md={12} padding={2}>
+                <SemesterForm />
+              </Grid>
+              <Grid item xs={12} md={12} padding={2}>
+                <Button onClick={handleClose} variant="contained">
+                  Add
+                </Button>
+              </Grid>
+            </Grid>
+          </Card>
+        </Grid>
+      </Modal>
     </>
   );
 }
